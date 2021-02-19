23b2ce24c46ae1a9d9ed289496c0107341c85ca7
unknown <fnumilat@gmail.com>
2021-02-18 18:53:34 -0600

    Created the React App.

---
+++ 40-positive-quotes-app/src/components/header.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="Header-Title">
+        <a>
+          Hello World!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
