b453ce5171da8ec25a1b75ef376e3471e35051c8
unknown <fnumilat@gmail.com>
2021-02-19 19:17:35 -0600

    Work done with the Header.

--- 40-positive-quotes-app/src/components/header.js
+++ 40-positive-quotes-app/src/components/header.js
@@ -1,13 +1,23 @@
 import '../App.css';
+import HeaderImage from '../images/header-image.jpg'
 
 function Header() {
   return (
     <div className="Header">
       <header className="Header-Title">
         <a>
-          Hello World!
+          40 POSITIVE QUOTES
         </a>
       </header>
+      <div className="Header-Image">
+        <img src={HeaderImage} className="Image"/>
+      </div>
+      <div className="Header-Paragraph">
+        <p className="Paragraph">Good and positive quotes make you feel positive, optimistic, energetic and optimistic, 
+        ready to set goals and take action. Their effect might continue for a little while or longer. 
+        To turn this attitude into a habit, you need to read quotes often and every day.
+        </p>
+      </div>
     </div>
   );
 }
