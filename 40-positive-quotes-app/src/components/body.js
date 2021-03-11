e447816809d8c560173b8ced7be6e26ac3f5b6c5
unknown <fnumilat@gmail.com>
2021-03-10 19:25:23 -0600

    Worked on the body.

--- 40-positive-quotes-app/src/components/body.js
+++ 40-positive-quotes-app/src/components/body.js
@@ -4,71 +4,325 @@ function Body() {
   return (
     <div className="Body">
 
+      {/* 1 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “Create a definite plan for carrying out your desire and begin at once, whether you are ready or not, to put this plan into action.”
         </p>
+        <p className="Quoting-From">- Napoleon Hill</p>
       </div>
       
+      {/* 2 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “All our dreams can come true… if we have the courage to pursue them.”
         </p>
+        <p className="Quoting-From">- Walt Disney</p>
       </div>
 
+      {/* 3 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “Hope is the thing with feathers that perches in the soul, and sings the tune, without the words and never stops at all.”
         </p>
+        <p className="Quoting-From">- Emily Dickenson</p>
       </div>
 
+      {/* 4 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “Aim for the moon. If you miss, you may hit a star.”
         </p>
+        <p className="Quoting-From">- W. Clement Stone</p>
       </div>
       
+      {/* 5 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “It does not matter how slowly you go, so long as you do not stop.”
         </p>
+        <p className="Quoting-From">- Confucius</p>
       </div>
 
+      {/* 6 */}
       <div className="Quote-Box">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="Quote-Paragraph">
+        “I find that the harder I work, the more luck I seem to have.”
         </p>
+        <p className="Quoting-From">- Thomas Jefferson</p>
       </div>
+
+      {/* 7 */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        “Today I will do what others won’t, so tomorrow I can accomplish what others can’t.”
+        </p>
+        <p className="Quoting-From">- Jerry Rice</p>
+      </div>
+
+      {/* 8 */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        “Don’t wait for something big to occur. Start where you are, with what you have, and that will always lead you into something greater.”
+        </p>
+        <p className="Quoting-From">- Mary Manin Morrissey</p>
+      </div>
+
+      {/* 9 */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        “Keep away from people who try to belittle your ambitions. Small people always do that, but the really great make you feel that you, too, can become great.”
+        </p>
+        <p className="Quoting-From">- Mark Twain</p>
+      </div>
+
+      {/* 10 */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        “The people who get on in this world are the people who get up and look for the circumstances they want, and if they can’t find them, make them.”
+        </p>
+        <p className="Quoting-From">- George Bernard Shaw</p>
+      </div>
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>   
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>    
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>   
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>    
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>   
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>  
+
+      {/*  */}
+      <div className="Quote-Box">
+        <p className="Quote-Paragraph">
+        
+        </p>
+        <p className="Quoting-From">- </p>
+      </div>    
 
     </div>
   );
