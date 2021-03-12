db21671a3da2aa003a46c251c63f8df28b6ee341
unknown <fnumilat@gmail.com>
2021-03-11 19:13:20 -0600

    Worked on Body and created the Footer.

--- 40-positive-quotes-app/src/components/body.js
+++ 40-positive-quotes-app/src/components/body.js
@@ -84,244 +84,244 @@ function Body() {
         <p className="Quoting-From">- George Bernard Shaw</p>
       </div>
 
-      {/*  */}
+      {/* 11 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Go confidently in the direction of your dreams! Live the life you have imagined.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Thoreau</p>
       </div>   
 
-      {/*  */}
+      {/* 12 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “I have not failed. I’ve just found 10,000 ways that won’t work.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Thomas A. Edison</p>
       </div>  
 
-      {/*  */}
+      {/* 13 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “You are never too old to set another goal or to dream a new dream.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Les Brown</p>
       </div>  
 
-      {/*  */}
+      {/* 14 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “We may encounter many defeats but we must not be defeated.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Maya Angelou</p>
       </div>  
 
-      {/*  */}
+      {/* 15 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Always make a total effort, even when the odds are against you.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Arnold Palmer</p>
       </div>  
 
-      {/*  */}
+      {/* 16 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “What you do today can improve all your tomorrows.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Ralph Marston</p>
       </div>  
 
-      {/*  */}
+      {/* 17 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “To forgive is the highest, most beautiful form of love. In return, you will receive untold peace and happiness.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Robert Muller</p>
       </div>  
 
-      {/*  */}
+      {/* 18 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Don’t be afraid to give up the good to go for the great.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- John D. Rockefeller</p>
       </div>  
 
-      {/*  */}
+      {/* 19 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Pursue some path, however narrow and crooked, in which you can walk with love and reverence.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Henry David Thoreau</p>
       </div>  
 
-      {/*  */}
+      {/* 20 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Everyone here has the sense that right now is one of those moments when we are influencing the future.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Steve Jobs</p>
       </div>    
 
-      {/*  */}
+      {/* 21 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        Positive thinking will let you do everything better than negative thinking will.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Zig Ziglar</p>
       </div>   
 
-      {/*  */}
+      {/* 22 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Failure is simply the opportunity to begin again, this time more intelligently.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Henry Ford</p>
       </div>  
 
-      {/*  */}
+      {/* 23 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Look deep into nature, and then you will understand everything better.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Albert Einstein</p>
       </div>  
 
-      {/*  */}
+      {/* 24 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “We can do anything we want to if we stick to it long enough.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Helen Keller</p>
       </div>  
 
-      {/*  */}
+      {/* 25 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “The only place success comes before work is in the dictionary.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Vince Lombardi</p>
       </div>  
 
-      {/*  */}
+      {/* 26 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Attitude is a little thing that makes a big difference.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Winston Churchill</p>
       </div>  
 
-      {/*  */}
+      {/* 27 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Motivation is everything. You can do the work of two people, but you can’t be two people. Instead, you have to inspire the next guy down the line and get him to inspire his people.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Lee Iacocca</p>
       </div>  
 
-      {/*  */}
+      {/* 28 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “With the new day come new strength and new thoughts.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Eleanor Roosevelt</p>
       </div>  
 
-      {/*  */}
+      {/* 29 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “The more tranquil a man becomes, the greater is his success, his influence, his power for good. Calmness of mind is one of the beautiful jewels of wisdom.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- James Allen</p>
       </div>  
 
-      {/*  */}
+      {/* 30 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “To accomplish great things, we must not only act, but also dream, not only plan, but also believe.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Anatole France</p>
       </div>    
 
-      {/*  */}
+      {/* 31 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Success comes from having dreams that are bigger than your fears.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Bobby Unser</p>
       </div>   
 
-      {/*  */}
+      {/* 32 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Norman Vincent Peale</p>
       </div>  
 
-      {/*  */}
+      {/* 33 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “here will be obstacles. There will be doubters. There will be mistakes. But with hard work, there are no limits.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Michael Phelps</p>
       </div>  
 
-      {/*  */}
+      {/* 34 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Never ask a man what he knows, but what he can do.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- George Lorimer</p>
       </div>  
 
-      {/*  */}
+      {/* 35 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “If you cannot do great things, do small things in a great way.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Napoleon Hill</p>
       </div>  
 
-      {/*  */}
+      {/* 36 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Be not afraid of life. Believe that life is worth living, and your belief will help create the fact.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- William James</p>
       </div>  
 
-      {/*  */}
+      {/* 37 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “The reason most people never reach their goals is that they don’t define them, or ever seriously consider them as believable or achievable. Winners can tell you where they are going, what they plan to do along the way, and who will be sharing the adventure with them.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Denis Watiley</p>
       </div>  
 
-      {/*  */}
+      {/* 38 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Don’t judge each day by the harvest you reap but by the seeds that you plant.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Robert Louis Stevenson</p>
       </div>  
 
-      {/*  */}
+      {/* 39 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “The great thing in this world is not so much where you stand, as in what direction you are moving.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Oliver Wendell Holmes</p>
       </div>  
 
-      {/*  */}
+      {/* 40 */}
       <div className="Quote-Box">
         <p className="Quote-Paragraph">
-        
+        “Though no one can go back and make a brand new start, anyone can start from now and make a brand new ending.”
         </p>
-        <p className="Quoting-From">- </p>
+        <p className="Quoting-From">- Carl Bard</p>
       </div>    
 
     </div>
---
+++ 40-positive-quotes-app/src/components/footer.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h1>Hi I am Footer!</h1>
+    </div>
+  );
+}
+
+export default Footer;
